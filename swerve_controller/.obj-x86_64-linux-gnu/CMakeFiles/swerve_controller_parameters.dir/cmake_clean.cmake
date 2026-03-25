9f28a7d64f8af3796afe7bbea28e7f4df02948bf
jclinton830 <jclinton830@gmail.com>
2026-03-25 14:23:49 +1100

    removing deb folders

--- swerve_controller/.obj-x86_64-linux-gnu/CMakeFiles/swerve_controller_parameters.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "include/swerve_controller/swerve_controller_parameters.hpp"
-  "include/swerve_controller_parameters.hpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/swerve_controller_parameters.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- swerve_controller/.obj-x86_64-linux-gnu/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jerome/gh_ws/src/ros2_controllers/swerve_controller
-# Build directory: /home/jerome/gh_ws/src/ros2_controllers/swerve_controller/.obj-x86_64-linux-gnu
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
